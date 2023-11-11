3a7681ca43ea9537064f7c94502f7c25928b354f
SK-WebApp <111104005+SK-WebApp@users.noreply.github.com>
2023-11-11 18:10:00 -0500

    Add files via upload
    update app.is ( error vs. err)

--- app.js
+++ app.js
@@ -3,5 +3,5 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js")
     .then((reg) => console.log("Service Worker Registered", reg))
-    .catch((error) => console.log("Service Worker Not Registered", err));
+    .catch((error) => console.log("Service Worker Not Registered", error));
 }
